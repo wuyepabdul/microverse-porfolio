171b840e42a2ce8b55d320f0a2cca80b405e4792
wuyepabdul <wuyepabdul@gmail.com>
2021-08-31 13:03:35 +0100

    Create a list containing Projects in app.js

--- app.js
+++ app.js
@@ -19,3 +19,42 @@ function closeMenuList() {
 hamburger.addEventListener('click', showMenu);
 menuClose.addEventListener('click', closeMenu);
 menuLinks.addEventListener('click', closeMenuList);
+
+let projects = [
+  {
+    name: 'Project 1',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    image: './images/works.png',
+    technologies: ['html', 'Ruby on Rails', 'css', 'Github'],
+    viewLive: 'link',
+    viewSource: 'https://github.com/wuyepabdul/microverse-porfolio',
+  },
+  {
+    name: 'Project 1',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    image: './images/works.png',
+    technologies: ['html', 'Ruby on Rails', 'css', 'Github'],
+    viewLive: 'link',
+    viewSource: 'https://github.com/wuyepabdul/microverse-porfolio',
+  },
+  {
+    name: 'Project 1',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    image: './images/works.png',
+    technologies: ['html', 'Ruby on Rails', 'css', 'Github'],
+    viewLive: 'link',
+    viewSource: 'https://github.com/wuyepabdul/microverse-porfolio',
+  },
+  {
+    name: 'Project 1',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    image: './images/works.png',
+    technologies: ['html', 'Ruby on Rails', 'css', 'Github'],
+    viewLive: 'link',
+    viewSource: 'https://github.com/wuyepabdul/microverse-porfolio',
+  },
+];
