6b0b5b68694f53bf5907a42b9b78a4a4805faf71
wuyepabdul <wuyepabdul@gmail.com>
2021-08-31 02:24:43 +0100

    Fix linter error

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const hamburger = document.querySelector(".hamburger");
-const menuContainer = document.querySelector(".menu-container");
-const menuClose = document.querySelector(".close-menu");
-const menuLinks = document.querySelector(".menu-links");
-
-hamburger.addEventListener("click", showMenu);
-menuClose.addEventListener("click", closeMenu);
-menuLinks.addEventListener("click", closeMenuList);
+const hamburger = document.querySelector('.hamburger');
+const menuContainer = document.querySelector('.menu-container');
+const menuClose = document.querySelector('.close-menu');
+const menuLinks = document.querySelector('.menu-links');
 
 function showMenu() {
-  menuContainer.style.display = "flex";
-  menuContainer.style.top = "0";
+  menuContainer.style.display = 'flex';
+  menuContainer.style.top = '0';
 }
 
 function closeMenu() {
-  menuContainer.style.top = "-100%";
+  menuContainer.style.top = '-100%';
 }
 
 function closeMenuList() {
-  menuContainer.style.top = "-100%";
+  menuContainer.style.top = '-100%';
 }
+
+hamburger.addEventListener('click', showMenu);
+menuClose.addEventListener('click', closeMenu);
+menuLinks.addEventListener('click', closeMenuList);
