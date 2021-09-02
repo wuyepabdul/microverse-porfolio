947158457cda66d6974116f07b15224de17c45c1
Danny Benshi <benshidanny11@gmail.com>
2021-09-02 14:03:22 +0200

    Add form validation

---
+++ formvalidation.js
@@ -0,0 +1,17 @@
+const contactMeForm = document.getElementById("contactme");
+
+function lowerCaseValidate(email) {
+  return /[a-z]/.test(email) && /[A-Z]/.test(email);
+}
+
+contactMeForm.addEventListener("submit", function (event) { 
+  event.preventDefault();
+  const ERROR_MESSAGE = "Email should be in  lower case";
+  const email = document.getElementById("email").value;
+  if(lowerCaseValidate(email)){
+    document.getElementById("p-error").innerHTML =ERROR_MESSAGE;
+  }else{
+      console.log(email)
+  }
+
+});
