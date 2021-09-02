749ce145340b67dc1ae85c67c3fa4faa01fdc1ef
wuyepabdul <wuyepabdul@gmail.com>
2021-09-02 16:28:30 +0100

    Style footer icons

--- formvalidation.js
+++ formvalidation.js
@@ -1,17 +1,14 @@
-const contactMeForm = document.getElementById("contactme");
+const contactMeForm = document.getElementById('contactme');
 
 function lowerCaseValidate(email) {
   return /[a-z]/.test(email) && /[A-Z]/.test(email);
 }
 
-contactMeForm.addEventListener("submit", function (event) { 
+contactMeForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const ERROR_MESSAGE = "Email should be in  lower case";
-  const email = document.getElementById("email").value;
-  if(lowerCaseValidate(email)){
-    document.getElementById("p-error").innerHTML =ERROR_MESSAGE;
-  }else{
-      console.log(email)
+  const ERROR_MESSAGE = 'Email should be in  lower case';
+  const email = document.getElementById('email').value;
+  if (lowerCaseValidate(email)) {
+    document.getElementById('p-error').innerHTML = ERROR_MESSAGE;
   }
-
 });
