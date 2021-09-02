ad02e9f05d8bf26d2c7f9803d9e5176411e484ee
Danny Benshi <benshidanny11@gmail.com>
2021-09-02 17:26:45 +0200

    Implement user data storage

--- formvalidation.js
+++ formvalidation.js
@@ -7,7 +7,11 @@ function lowerCaseValidate(email) {
 contactMeForm.addEventListener("submit", function (event) { 
   event.preventDefault();
   const ERROR_MESSAGE = "Email should be in  lower case";
-  const email = document.getElementById("email").value;
+  const email = document.getElementById('useremail').value;
+  const name = document.getElementById('username').value;
+  const message = document.getElementById('usermessage').value;
+  const user = { name, email, message };
+  localStorage.setItem('userData', JSON.stringify(user));
   if(lowerCaseValidate(email)){
     document.getElementById("p-error").innerHTML =ERROR_MESSAGE;
   }else{
