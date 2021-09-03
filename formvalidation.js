6aa8c2d5c87c8e1c2db2ba5411160b9368aa99f7
wuyepabdul <wuyepabdul@gmail.com>
2021-09-03 10:18:46 +0100

    Make text visible in input fields

--- formvalidation.js
+++ formvalidation.js
@@ -1,21 +1,29 @@
-const contactMeForm = document.getElementById("contactme");
+const contactMeForm = document.getElementById('contactme');
 
 function lowerCaseValidate(email) {
-  return /[a-z]/.test(email) && /[A-Z]/.test(email);
+  return /[A-Z]/.test(email);
 }
 
-contactMeForm.addEventListener("submit", function (event) { 
+contactMeForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  const ERROR_MESSAGE = "Email should be in  lower case";
+  const ERROR_MESSAGE = 'Email should be in  lower case';
   const email = document.getElementById('useremail').value;
   const name = document.getElementById('username').value;
   const message = document.getElementById('usermessage').value;
   const user = { name, email, message };
   localStorage.setItem('userData', JSON.stringify(user));
-  if(lowerCaseValidate(email)){
-    document.getElementById("p-error").innerHTML =ERROR_MESSAGE;
-  }else{
-      console.log(email)
+  if (lowerCaseValidate(email)) {
+    document.getElementById('p-error').innerHTML = ERROR_MESSAGE;
+  } else {
+    contactMeForm.submit();
   }
+});
 
+window.addEventListener('load', () => {
+  if (localStorage.length > 0) {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    document.getElementById('useremail').value = userData.email;
+    document.getElementById('username').value = userData.name;
+    document.getElementById('usermessage').value = userData.message;
+  }
 });
